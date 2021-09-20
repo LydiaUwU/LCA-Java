eab74794cd057c5be22509e2e58bf90a0b382acb
Lydia <lydiamacbride@protonmail.com>
2021-09-20 21:28:38 +0100

    File layout

---
+++ src/BST.java
@@ -0,0 +1,13 @@
+/**
+ * @Author: Lydia MacBride, 19333944
+ *
+ * Implementation of a Binary Search Tree, Nodes and a Lowest Common Ancestor algorithm
+ */
+
+public class BST {
+
+}
+
+class Node {
+
+}
---
+++ src/Main.java
@@ -0,0 +1,11 @@
+/**
+ * @Author: Lydia MacBride, 19333944
+ *
+ * Main class to run an example program to demonstrate the functionality of my implementation of an LCA algorithm.
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}

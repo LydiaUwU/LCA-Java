08ecefffd049fb1b66c1b1ddd7940ff6e2c12998
Lydia <lydiamacbride@protonmail.com>
2021-09-21 18:43:59 +0100

    Final implementation of BST, insertion, searching and lowest common ancestor.

--- src/BST.java
+++ src/BST.java
@@ -1,13 +1,103 @@
+import java.util.ArrayList;
+
 /**
  * @Author: Lydia MacBride, 19333944
  *
  * Implementation of a Binary Search Tree, Nodes and a Lowest Common Ancestor algorithm
+ * Heavily based on these posts:
+ *  - https://www.geeksforgeeks.org/binary-search-tree-set-1-search-and-insertion/
+ *  - https://www.geeksforgeeks.org/lowest-common-ancestor-in-a-binary-search-tree/
  */
 
+//BST Class
 public class BST {
+    Node root;
 
+    //Binary standard tree constructor
+    BST() {
+        root = null;
+    }
+
+    //Call getNode()
+    Node getNode(int key) {
+        return getNode(root, key);
+    }
+
+    //Get node from tree
+    Node getNode(Node root, int key) {
+        //Base cases where root is null or search matches root
+        if (root == null || root.key == key) {
+            return root;
+        }
+
+        //Key greater than root's key
+        if (key > root.key) {
+            getNode(root.right, key);
+        }
+
+        //Key less than root's key
+        return getNode(root.left, key);
+    }
+
+    //Call addNode()
+    void addNode(int key)
+    {
+        root = addNode(root, key);
+    }
+
+    //Add node to tree
+    Node addNode(Node node, int key) {
+        //Return new node if tree is empty
+        if (node == null) {
+            node = new Node(key);
+            return node;
+        }
+
+        //Else recur through tree
+        if (key < root.key) {
+            node.left = addNode(node.left, key);
+        } else if (key > root.key) {
+            node.right = addNode(node.right, key);
+        }
+
+        //Return unchanged node
+        return node;
+    }
+
+    //Calls lca(Node, int, int)
+    Node lca(int n1, int n2) {
+        return lca(root, n1, n2);
+    }
+
+    //Find lowest common ancestor of two nodes
+    Node lca(Node node, int n1, int n2)
+    {
+        if (node == null) {
+            return null;
+        }
+
+        // If both n1 and n2 are smaller than root, then LCA lies in left
+        if (node.key > n1 && node.key > n2) {
+            return lca(node.left, n1, n2);
+        }
+
+        // If both n1 and n2 are greater than root, then LCA lies in right
+        if (node.key < n1 && node.key < n2) {
+            return lca(node.right, n1, n2);
+        }
+
+        return node;
+    }
 }
 
+//Node Class
 class Node {
+    int key;
+    Node right, left;
 
+    //Node constructor
+    Node(int key) {
+        this.key = key;
+        right = left = null;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,24 @@
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        //Initialise tree
+        BST tree = new BST();
+
+        /* Let us create following BST
+              50
+           /     \
+          30      70
+         /  \    /  \
+        20   40  60   80 */
+        tree.addNode(50);
+        tree.addNode(30);
+        tree.addNode(20);
+        tree.addNode(40);
+        tree.addNode(70);
+        tree.addNode(60);
+        tree.addNode(80);
+
+        //Example lca() call
+        System.out.println("LCA of 20 and 40: " + tree.lca(20, 40).key);
     }
 }
